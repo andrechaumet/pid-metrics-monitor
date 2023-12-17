1a6c45b9ceb084cd91f95d9773ddb8c50377cb0f
Andre <andrechaumetcutuli@gmail.com>
2023-12-17 18:00:24 -0300

    Added basic models structs

--- model/metric_model.go
+++ model/metric_model.go
@@ -1,1 +1,17 @@
 package model
+
+import (
+	"time"
+)
+
+type MyStruct struct {
+	ID                int64
+	StartTime         time.Time
+	LastUpdate        time.Time
+	CurrentIterations int64
+	CurrentSpeed 	  string
+	TotalIterations   int64
+	Percentage        float64
+	LapsedTime		  int64
+	ExpectedTime	  time.Time	
+}
--- model/pid_model.go
+++ model/pid_model.go
@@ -1,1 +1,14 @@
 package model
+
+import (
+	"time"
+)
+
+type TaskModel struct {
+	ID                int64
+	StartTime         time.Time
+	LastUpdate        time.Time
+	CurrentIterations int64
+	TotalIterations   int64
+	Logs 			  []string
+}
