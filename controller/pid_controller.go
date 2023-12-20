bfe51ee9102fde2dcef64213ab56ea5d8d7d6010
Andre <andrechaumetcutuli@gmail.com>
2023-12-20 10:13:30 -0300

    Added pid_controller basic structure

--- controller/pid_controller.go
+++ controller/pid_controller.go
@@ -1,1 +1,32 @@
 package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"pid-metrics-monitor/service"
+	"pid-metrics-monitor/model"
+ )
+ 
+ func CreatePID(c *gin.Context) {
+	var pidModel model.PidModel
+
+	if err := c.ShouldBindJSON(&pidModel); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	service.Save(pidModel)
+
+	c.JSON(http.StatusCreated, gin.H{"message": "PID creado"})
+}
+ 
+ func UpdatePID(c *gin.Context) {
+	pidID := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{"message": "PID actualizado", "id": pidID})
+ }
+ 
+ func CreatePIDLog(c *gin.Context) {
+	pidID := c.Param("id")
+	c.JSON(http.StatusCreated, gin.H{"message": "Log creado para PID", "id": pidID})
+ }
+
+ func handleBody(pid *model.PidModel)
